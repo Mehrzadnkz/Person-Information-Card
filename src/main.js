77638f6048fabe95207262c30cd8bc819a0ce7dc
Mehrzad_Nikzad <156532691+Mehrzadnkz@users.noreply.github.com>
2025-07-19 09:21:50 +0330

    Add API

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ document.querySelector("#app").innerHTML = `
   <!-- Header -->
   <div class="header">
     <div class="logo-div" id="header-elements">
-      <div class="header-logo bg-(url(${Logo}))">
+      <div class="header-logo">
+        <img src="${Logo}" alt="Logo" />
       </div>
     </div>
 
@@ -46,11 +47,30 @@ document.querySelector("#app").innerHTML = `
   </main>
 `;
 
+
+const userCardcontainer = document.getElementsByClassName("user-cards");
 fetch("https://jsonplaceholder.typicode.com/users")
   .then(response => response.json())
-  .then(data => {
-    // console.log(data);
+  .then(users => {
+    const usercard = document.createElement("div");
+    if (users.length % 2 == 0) {
+      console.log("2");
+    }
+    else if (users.length % 3 == 0) {
+      console.log("3");
+    }
+    usercard.classList.add("user-card", "border-2", "border-orange-500", "rounded", "p-8", "m-2");
+    usercard.innerText = " Meghdar Kol User Hay Sabt Shode : " + users.length;
+    console.log("%c Meghdar Kol User Hay Sabt Shode : ", 'color: orange; font-size: 3.5mm;', + users.length);
+    userCardcontainer[0].appendChild(usercard);
   })
   .catch(error => {
     console.error("Error fetching data:", error);
   });
+
+
+//   for (let i = 0; i < data.length; i++) {
+//   console.log("User Shomare : " + (i + 1));
+//   console.log(JSON.stringify(data[i]));
+// }
+// console.log("%c Meghdar Kol User Hay Sabt Shode : ", 'color: orange; font-size: 3.5mm;', + data.length);
